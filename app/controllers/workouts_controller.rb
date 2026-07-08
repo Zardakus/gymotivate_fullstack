e8288bc105a853b0d245a30c70b327db46019868
Pedro Paulo <pedroonlinerm@hotmail.com>
2026-07-07 23:21:45 -0300

    feat: exclusao reativa de treinos com turbo streams e dom_id

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,4 +1,7 @@
 class WorkoutsController < ApplicationController
+# Executa o método set_workout para carregar a ficha antes de rodar as ações específicas
+before_action :set_workout, only: [:destroy]
+
   def index
     # Usando Eager Loading (includes) para matar o problema de N+1 queries
     @workouts = Workout.includes(:trainer, :member).all
@@ -23,8 +26,24 @@ class WorkoutsController < ApplicationController
     end
   end
 
+  def destroy
+    authorize @workout
+    @workout.destroy
+
+    # Respondemos dizendo ao Rails para procurar um arquivo de fluxo ou rodar inline
+    respond_to do |format|
+      format.html { redirect_to root_path, notice: "Treino removido." }
+      # Se a requisição for do Turbo, ele executa essa linha:
+      format.turbo_stream { render turbo_stream: turbo_stream.remove(helpers.dom_id(@workout))}
+    end
+  end
+
   private
-  # O famoso Strong Parameters que vimos na Fase 2
+
+  def set_workout
+    @workout = Workout.find(params[:id])
+  end
+
   def workout_params
     params.require(:workout).permit(:name, :description, :trainer_id, :member_id)
   end
